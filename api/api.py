73b34f5e02fac1d6d340262e694762641bb17736
Waziri Shebogholo <shebogholo@gmail.com>
2019-08-08 10:17:31 +0300

    updated

--- api/api.py
+++ api/api.py
@@ -6,7 +6,8 @@ from sklearn.externals import joblib
 def model_prediction(inputs):
     features = np.array(inputs).reshape(-1, 1)
     try:
-        model = joblib.load('static/weather_model.pkl')
+        with open('static/weather_model.pkl', 'rb') as file:
+            model = joblib.load(file)
     except:
         print('Error: Application failed')
         sys.exit(0)
