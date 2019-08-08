3f154ede9c36d230e0dca46228a67cbf6a273839
Waziri Shebogholo <shebogholo@gmail.com>
2019-08-08 09:30:52 +0300

    added regression model prediction

--- api/app.py
+++ api/app.py
@@ -2,15 +2,19 @@ import sys
 import numpy as np
 from flask import Flask
 from sklearn.externals import joblib
-from flask import render_template, request, jsonify, redirect, url_for
+from flask import render_template, request
 
 app = Flask(__name__)
 
-try:
-    model = joblib.load('static/logistic_regression_model.pkl')
-except:
-    print('Error: Application failed')
-    sys.exit(0)
+
+def model_prediction(inputs):
+    features = np.array(inputs).reshape(-1, 1)
+    try:
+        model = joblib.load('static/weather_model.pkl')
+    except:
+        print('Error: Application failed')
+        sys.exit(0)
+    return model.predict(features)
 
 
 @app.route('/')
@@ -18,19 +22,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['post', 'get'])
 def predict():
-    # args = request.args
-    # required = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
-    # difference = set(required).difference(set(args.keys()))
-    # if len(difference):
-    #     return 'Error: Missing arguments'
-
-    # features = np.array().reshape(1, -1)
-    # result = model.predict()[:, 1][0]
-    # return redirect(url_for('index'))
-    return 'Waziri Shebogholo'
-    # return jsonify({'Predicted temperature is: ': result})
+    min_temperature = request.form.get('min_temperature')
+    data = np.array(int(min_temperature))
+    return 'The model prediction is {:.2f}'.format(model_prediction(data).flatten()[0])
 
 
 if __name__ == '__main__':
