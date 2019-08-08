896beaa957c02c67fef04552fd4fcd61b4bb54bc
Waziri Shebogholo <shebogholo@gmail.com>
2019-08-08 10:26:41 +0300

    updated flask

--- api/api.py
+++ api/api.py
@@ -2,11 +2,13 @@ import sys
 import numpy as np
 from sklearn.externals import joblib
 
+file_name = 'static/weather_model.pkl'
+
 
 def model_prediction(inputs):
     features = np.array(inputs).reshape(-1, 1)
     try:
-        with open('static/weather_model.pkl', 'rb') as file:
+        with open(file_name, 'rb') as file:
             model = joblib.load(file)
     except:
         print('Error: Application failed')
